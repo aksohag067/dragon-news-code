ad357cf7ead68d480f51472c921db2ad92118d5c
ak sohag <aksohag067@gmail.com>
2024-11-15 20:17:31 +0600

    News portal

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,12 @@
+
+
+const LatestNews = () => {
+    return (
+        <div className="flex gap-2 items-center">
+            <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quis quibusdam delectus maxime nemo aspernatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, ducimus.</p>   
+        </div>
+    );
+};
+
+export default LatestNews;
