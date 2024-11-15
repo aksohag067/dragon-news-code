0733a6503a6675e18c2c7e11c444cb40bd12ec38
ak sohag <aksohag067@gmail.com>
2024-11-15 23:39:26 +0600

    Updated

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,10 +1,22 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 
 const LatestNews = () => {
     return (
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center bg-base-200 p-2">
             <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quis quibusdam delectus maxime nemo aspernatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, ducimus.</p>   
+            <Marquee pauseOnHover={true} speed={100} className="space-x-10">
+                <Link to='/news'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ipsa?
+                </Link>
+                <Link to='/news'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ipsa?
+                </Link>
+                <Link to='/news'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ipsa?
+                </Link>
+            </Marquee>  
         </div>
     );
 };
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,18 +1,32 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
-
+import LeftNavbar from "../components/layout-components/LeftNavbar";
+import RightNav from "../components/layout-components/RightNav";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <header>
-            <Header />
-            <section className="w-11/12 mx-auto">
-                <LatestNews />
-            </section>
-            </header>
-        </div>
-    );
+  return (
+    <div className="font-poppins">
+      <header>
+        <Header />
+        <section className="w-11/12 mx-auto">
+          <LatestNews />
+        </section>
+      </header>
+      <nav className="w-11/12 mx-auto py-2">
+        <Navbar />
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">
+            <LeftNavbar />
+        </aside>
+        <section className="col-span-6">Main content</section>
+        <aside className="col-span-3">
+            <RightNav />
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default HomeLayout;
